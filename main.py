c2585ac52523132880c0ad19a97d10ecfa6aa78e
Raed2001 <raed00635@gmail.com>
2022-04-27 14:53:34 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import os
+import subprocess
+import cgi
+import cgitb
+import sys
+
+# os.system("mkdir new_directory")
+os.getpid()
+##os.chmod("u", "+rwx")
+os.chdir(r"C:\Users")
+
+p = subprocess.Popen(
+    ['dir'],
+    stdout=subprocess.PIPE, shell=True)
+output = p.communicate()[0]
+print(output)
